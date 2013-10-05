a99860cdc1474197b116b40a1a6dee681690f3a3
mehdi <mehdi@mehdi-Compaq-610>
2013-10-05 14:31:18 +0000

    new

--- GAE-WebApp/src/main/webapp/js/api/flickr.js
+++ GAE-WebApp/src/main/webapp/js/api/flickr.js
@@ -3,38 +3,68 @@ var flickr_key= '88d30455b41ff5aab043a29dc3bbd53c';
 //flickr_secret= 0b4f276202e7253b
 
 //http://www.flickr.com/services/api/flickr.photos.search.html
+//http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg -->
 
 var photos="";
 
-function searchPhoto(keyword) {
+function searchPhoto(keyword, page) {
      var method = 'flickr.photos.search';
      $.ajax({
          url: flickr_url + method + '&format=json'+ '&api_key=' + flickr_key,
          dataType: 'jsonp',
-         data: {'text': keyword,
-                //'tags': (keyword, 'Actor'),
-                //'tag_mode': "", 
-                //'content_type':3,
-                //'in_gallery': true,
-                'sort':'relevance'
+         data: {
+                'text': keyword,
+                'sort':'relevance',
+                'page': page,
                },
          type: 'GET',
          jsonpCallback: 'jsonFlickrApi',
          success: function(json){
-                 if (json.photos) {
-                    photos = json.photos;
-                    progressbar("20");
-                    //displayPhotoResultNumber(json);
-                    //var photo = json.photos.photo[10];
-                    //console.log(json.photos);
-                    //console.log("http://farm2.staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+".jpg");                
+                  if (json.photos) {
+                     photos = json.photos;
+                     progressbar("20");
+                     console.log(json);                
                  }       
          }
     });
  
 }
 
-function searchPhotosNumber() {
+
+function displayPhotosNumber() {
     if(photos!="") {return photos.total;}
     else return "0";
+}
+
+function displayPagesNumber() {
+    if(photos!="") {return photos.pages;}
+    else return "0";
+}
+
+function displayPhoto(i) {
+    var photo = photos.photo[i];
+    console.log(photo);
+    var url = "http://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+"_z.jpg";
+    console.log(url);
+    return url;
+}
+
+function displayThumbnail(i) {
+    var photo = photos.photo[i];
+    var url = "http://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+"_t.jpg";
+    console.log(url);
+    return url;
+}
+
+function displayTitle(i) {
+    var photo = photos.photo[i];
+    return photo.title;
+}
+
+function renderImageListview(start,end) {
+      var html="";
+      for(var i=start; i<=end; i++) {               
+	  html+='<li><a href='+displayPhoto(i)+' rel="external">'+'<img src='+displayThumbnail(i)+' alt='+displayTitle(i)+'/></a>'+'</li>'
+      }
+      return html;
 }
